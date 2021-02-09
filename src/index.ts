63a49f558b69bde2a9a504e3334d2b7f00faeb9f
Anya Fadeeva <anyasimba@yandex.ru>
2021-02-09 18:40:43 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+const $$events = Symbol('events')
+
+type IEventEmitter = {
+    [$$events]?: { [key: string]: ((...args: any[]) => void)[] }
+}
+
+export class EventEmitter {
+    on(this: IEventEmitter, ev: string, fn: (...args: any) => void) {
+        if (!this[$$events]) {
+            this[$$events] = {}
+        }
+        const events = this[$$events]!
+        if (!events[ev]) {
+            events[ev] = []
+        }
+
+        events[ev].push(fn)
+
+        return () => {
+            events[ev] = events[ev].filter(eventFn => fn !== eventFn)
+            if (events[ev].length === 0) {
+                delete events[ev]
+            }
+            if (Object.keys(events).length === 0) {
+                delete this[$$events]
+            }
+        }
+    }
+
+    emit(this: IEventEmitter, ev: string, ...args: any[]) {
+        const events = this[$$events] && this[$$events]![ev]
+        if (events) {
+            events.forEach(fn => {
+                fn.call(null, ...args)
+            })
+        }
+    }
+}
+
+export function FunctionEventEmitter<T extends any[], R>(fn: (...args: T) => R) {
+    Object.setPrototypeOf(fn, EventEmitter.prototype)
+    return fn as ((...args: T) => R) & EventEmitter
+}
